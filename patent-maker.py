b609491fd4fd66052d3e150276d2dd62855fa235
SHAI PEREDNIK <shaiss@gmail.com>
2020-07-16 17:56:10 +0000

    updated code for text

--- patent-maker.py
+++ patent-maker.py
@@ -1,38 +1,32 @@
-import inkex       # Required
-import simplestyle # will be needed here for styles support
-import os          # here for alternative debug method only - so not usually required
-# many other useful ones in extensions folder. E.g. simplepath, cubicsuperpath, ...
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 
-from math import cos, sin, radians
+import inkex
 
-class Myextension(inkex.Effect): # choose a better name
-    
+class MyExtensionName(inkex.Effect):
     def __init__(self):
-        " define how the options are mapped from the inx file "
-        inkex.Effect.__init__(self) # initialize the super class
-
-    def add_text(self, node, text, position, text_height=12):
-    #Create and insert a single line of text into the svg under node.
-        line_style = {'font-size': '%dpx' % text_height, 'font-style':'normal', 'font-weight': 'normal',
-            'fill': '#F6921E', 'font-family': 'Bitstream Vera Sans,sans-serif',
-            'text-anchor': 'middle', 'text-align': 'center'}
-        line_attribs = {inkex.addNS('label','inkscape'): 'Annotation',
-            'style': simplestyle.formatStyle(line_style),
-            'x': str(position[0]),
-            'y': str((position[1] + text_height) * 1.2)
-        }
-        line = inkex.etree.SubElement(node, inkex.addNS('text','svg'), line_attribs)
-        line.text = text
+        inkex.Effect.__init__(self)
+        self.arg_parser.add_argument('--patenturl', action='store', type=str, dest='patent_text',
+                                     default=None, help='Helper text for this option')
 
     def effect(self):
-        topgroup = inkex.etree.SubElement(self.current_layer, 'g', g_attribs )
-    
+        text_type = self.options.patent_text
+        text = self._modifyTextNode(text_type)
+        # text = text_type
 
-        note = "poop"
-        text_height = 12
-        # position above
-        y = - 22
-        self.add_text(topgroup, note, [0,y], text_height)
+    def _main_function(self, option):
+           return text_type
+    
+    def _modifyTextNode(self, text_type):
+        svg = self.document.getroot()
+        current_selected = self.svg.selected
+        if current_selected:
+            for id_, node in current_selected.items():
+                if node.tag == inkex.addNS('text', 'svg'):
+                    tspan = node.getchildren()[0]
+                    text = self._getSampleText(text_type)
+                    tspan.text = text
 
 if __name__ == '__main__':
-    Myextension().run()
+    MyExtension = MyExtensionName()
+    MyExtension.run()
