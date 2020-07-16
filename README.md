6f4138344f4ce8edcbc6d4fdabf6d21064ea7175
SHAI PEREDNIK <shaiss@gmail.com>
2020-07-16 14:23:13 +0000

    doesn't work
    text version that doesn't work

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # patent-maker
+
+Requires this fork of the google patent scraper: https://github.com/mluttrell/google_patent_scraper
+
--- patent-maker.py
+++ patent-maker.py
@@ -1,40 +1,38 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-Example of extensions template for inkscape
-'''
-# standard library
-import locale, os, sys, tempfile, webbrowser, math
-try:
-    from subprocess import Popen, PIPE
-    bsubprocess = True
-except:
-    bsubprocess = False
-# local library
-import inkex, simplestyle, simpletransform
+import inkex       # Required
+import simplestyle # will be needed here for styles support
+import os          # here for alternative debug method only - so not usually required
+# many other useful ones in extensions folder. E.g. simplepath, cubicsuperpath, ...
 
- 
-patenturl ="poop4"
+from math import cos, sin, radians
 
-#self.options.patenturl
+class Myextension(inkex.Effect): # choose a better name
+    
+    def __init__(self):
+        " define how the options are mapped from the inx file "
+        inkex.Effect.__init__(self) # initialize the super class
 
-f = open("demofile4.txt", "w")
-f.write(patenturl)
-f.close()
+    def add_text(self, node, text, position, text_height=12):
+    #Create and insert a single line of text into the svg under node.
+        line_style = {'font-size': '%dpx' % text_height, 'font-style':'normal', 'font-weight': 'normal',
+            'fill': '#F6921E', 'font-family': 'Bitstream Vera Sans,sans-serif',
+            'text-anchor': 'middle', 'text-align': 'center'}
+        line_attribs = {inkex.addNS('label','inkscape'): 'Annotation',
+            'style': simplestyle.formatStyle(line_style),
+            'x': str(position[0]),
+            'y': str((position[1] + text_height) * 1.2)
+        }
+        line = inkex.etree.SubElement(node, inkex.addNS('text','svg'), line_attribs)
+        line.text = text
 
-#SVG element generation routine
-def draw_SVG_square((w,h), (x,y), parent):
+    def effect(self):
+        topgroup = inkex.etree.SubElement(self.current_layer, 'g', g_attribs )
+    
 
-    style = {   'stroke'        : 'none',
-                'stroke-width'  : '1',
-                'fill'          : '#000000'
-            }
-                
-    attribs = {
-        'style'     : simplestyle.formatStyle(style),
-        'height'    : str(h),
-        'width'     : str(w),
-        'x'         : str(x),
-        'y'         : str(y)
-            }
-    circ = inkex.etree.SubElement(parent, inkex.addNS('rect','svg'), attribs )
+        note = "poop"
+        text_height = 12
+        # position above
+        y = - 22
+        self.add_text(topgroup, note, [0,y], text_height)
+
+if __name__ == '__main__':
+    Myextension().run()
